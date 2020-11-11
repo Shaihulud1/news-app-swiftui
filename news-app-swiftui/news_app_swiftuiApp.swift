5fa0eab7252de1b8419fc6053f3d214fcf0bc5b0
Ilia <godofcode1@icloud.com>
2020-11-11 04:44:38 +0400

    Initial Commit

---
+++ news-app-swiftui/news_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  news_app_swiftuiApp.swift
+//  news-app-swiftui
+//
+//  Created by Илья Дернов on 11.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct news_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
